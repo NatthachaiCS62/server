4c64fd1e20b8ab31a2254ebf1f86719bad54a750
NatthachaiCS62 <137139607+NatthachaiCS62@users.noreply.github.com>
2024-03-19 16:33:05 +0700

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,7 @@ const { create, list } = require('../controllers/product');
 
 router.post('/products', create);
 router.get("/products", list);
+router.put("/products/:id", update);
+router.delete("/products/:id", remove);
 
 module.exports = router;
