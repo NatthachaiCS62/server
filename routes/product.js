d104f0b26a4b0b1580e73f6154dd6c48db5240fa
NatthachaiCS62 <137139607+NatthachaiCS62@users.noreply.github.com>
2024-03-19 17:05:11 +0700

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { create, list } = require('../controllers/product');
+const { create, list, update, remove } = require('../controllers/product');
 
 router.post('/products', create);
 router.get("/products", list);
 router.put("/products/:id", update);
 router.delete("/products/:id", remove);
+
 
 module.exports = router;
